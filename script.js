e4e044b3f926e6302688adb1a0e31cf82080fa8e
mukung26 <91122120+mukung26@users.noreply.github.com>
2025-03-04 17:07:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var modal = document.getElementById("imageModal");
+    var modalImg = document.getElementById("modalImage");
+    var parcelImg = document.getElementById("parcelImage");
+    var closeBtn = document.querySelector(".close");
 
+    parcelImg.onclick = function () {
+        modal.style.display = "flex";
+        modalImg.src = parcelImg.src;
+    };
+
+    closeBtn.onclick = function () {
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    };
+});
